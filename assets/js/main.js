f1d6b219ed0bba4027726e7da487ab8e4396611c
Giovanni <gio.fon@gmail.com>
2018-08-28 22:02:49 +0200

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 $(function(){   //doc ready
 
+
+    Handlebars.registerHelper('formatDate', function (date, format) {
+        var mmnt = moment(date);
+        return mmnt.format(format);
+    });
+
    $.ajax("./data/curriculum.json").done(function(curriculum){
        $('#container').html(compiledTemplate(curriculum));
    }); 
